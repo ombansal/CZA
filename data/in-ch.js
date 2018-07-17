3d7d236e934b893e849e628e26a5bcaa8fb82d65
ombansal <om.bansal05@gmail.com>
2018-07-17 14:16:05 +0100

    update webpage
    added new pages

---
+++ data/in-ch.js
@@ -0,0 +1,22 @@
+Highcharts.maps["in-ch"]= {
+    "title":"India",
+    "version":"1.1.2",
+    "type":"FeatureCollection",
+    "copyright":"Copyright (c) 2015 Highsoft AS, Based on data from Natural Earth",
+    "copyrightShort":"Natural Earth",
+    "copyrightUrl":"http://www.naturalearthdata.com",
+    "crs": {
+        "type":"name",
+        "properties": {
+            "name": "urn:ogc:def:crs:EPSG:24373"
+        }
+    },
+    "hc-transform": {
+        "default": {
+            "crs": "+proj=lcc +lat_1=19 +lat_0=19 +lon_0=80 +k_0=0.99878641 +x_0=2743195.592233322 +y_0=914398.5307444407 +a=6377299.36559538 +b=6356098.359005156 +to_meter=0.9143985307444408 +no_defs", "scale": 0.000208805754963, "jsonres": 15.5, "jsonmarginX": -999, "jsonmarginY": 9851.0, "xoffset": 1676654.55745, "yoffset": 3026618.02308
+        }
+    }
+    ,"features":[
+  {"type":"Feature","id":"IN.CH","properties":{"hc-group":"admin1","hc-middle-x":0.74, "ID_1": 4,"hc-middle-y":0.29,"hc-key":"in-ch","hc-a2":"CH","labelrank":"9","hasc":"IN.CH","alt-name":null,"woe-id":"20070456","subregion":null,"fips":"IN05","postal-code":"CH","name":"Chandigarh","country":"India","type-en":"Union Territory","region":"North","longitude":"76.76049999999999","woe-name":"Chandigarh","latitude":"30.7452","woe-label":"Chandigarh, IN, India","type":"Union Territor"},"geometry":{"type":"Polygon","coordinates":[[[1967,7568],[1970,7548],[1969,7529],[1933,7566],[1967,7568]]]}}
+]
+}
